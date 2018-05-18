7b4f4d8c559e2366781b143c88eb0efa159cc14e
谢峻 <jy.xj@alibaba-inc.com>
2018-05-18 17:31:52 +0800

    update

--- scrapy.py
+++ scrapy.py
@@ -1,7 +1,7 @@
 import requests
 import math
 from bs4 import BeautifulSoup
-
+import re
 from bokeh.io import output_notebook, show # 引入初始化
 from bokeh.plotting import figure #引入图表界面
 from bokeh.models import ColumnDataSource, HoverTool, FactorRange, LabelSet
@@ -13,47 +13,56 @@ from bokeh.transform import factor_cmap
 from bokeh.models.glyphs import VBar
 
 
-url = "https://sz.lianjia.com/ershoufang/nanshanqu/l3a3p4/"
-count = 0
 
-def grab(url):
-    global count #使用全局变量
+
+# 爬取特定区域内挂牌7天以内的房源
+def grab(url, last_day):
     r = requests.get(url)
     soup = BeautifulSoup(r.content, 'lxml')
     g_data = soup.find_all("div", {"class", "info clear"})
-    print(g_data[0].contents)
+    
     for item in g_data:
-        try:
-            yard = item.contents[1].find_all('a')[0].text
-        except:
-            pass
-        try:
-            price = item.contents[5].find_all('span')[1].text
-        except:
-            pass
-        try:
-            address = item.contents[4].find_all('span', {'class', 'subway'})[0].text
-        except:
-            pass
-        count +=1
-
-        print(yard+ '|' + price + '|') #打印出 小区，价格，地址
-
+        day = item.contents[3].text.split('/ ')[2]
+        if day == '刚刚发布' or int(re.match('\d+', day).group()) < last_day:
+            try:
+                yard = item.contents[1].find_all('a')[0].text
+            except:
+                pass
+            try:
+                price = item.contents[5].find_all('span')[1].text
+            except:
+                pass
+            try:
+                address = item.contents[4].find_all('span', {'class', 'subway'})[0].text
+            except:
+                pass
+            try:
+                time = item.contents[3].text.split('/ ')[2]
+            except:
+                pass
+            print(yard+ '|' + price + '|' + time + '|') #打印出 小区，价格，地址
 
 #先获取总页数，再以循环的方式获取列表
-def getTotal(url):
-    r = requests.get(url)
-    soup = BeautifulSoup(r.content, 'lxml')
-    total = soup.find('h2', {'class', 'total fl'})
-    number = int(total.find('span').text)
-    page = math.ceil(number/30)
+def getTotal(day):
+    page = 5 # 爬的总页数
     count = 1
-    while count<=page:
-        grab("https://sz.lianjia.com/ershoufang/nanshanqu/pg"+str(count)+"l3a3p4/")
-        count += 1
+    area_list = [
+        'https://sz.lianjia.com/ershoufang/luohuqu/pg', # 罗湖
+        'https://sz.lianjia.com/ershoufang/futianqu/pg', # 福田
+        'https://sz.lianjia.com/ershoufang/nanshanqu/pg', # 南山
+        'https://sz.lianjia.com/ershoufang/baoanqu/pg'] # 宝安
+    for link in area_list:
+        while count<=page:
+            href = link + str(count) +"co32a3a4/"
+            print(href)
+            grab(href, day)
+            count += 1
+        count = 1    
 
-# 爬取特定条件下的房源
-#grab(url)
+# 爬取 7天内 新上架的房源
+getTotal(7)
+
+
 
 # 爬取特定区域的房源总数 （70~90，90~110 不考虑价格）
 def grab_area_house_number():
@@ -75,8 +84,8 @@ def grab_area_house_number():
     label_name = ['罗湖','福田','南山','宝安']
 
     diagram = figure(x_range     = label_name, 
-                    plot_height  = 250, 
                     y_range      = [0, 2000], 
+                    plot_height  = 250, 
                     title        = title, 
                     tools        = 'wheel_zoom',
                     y_axis_label = '房源数量')
@@ -108,7 +117,157 @@ def grab_area_house_number():
     show(diagram)
     
 
-grab_area_house_number()
+#grab_area_house_number()
+
+
+# 30 天内的成交数据
+def getDoneTransaction():
+    url    = 'https://sz.lianjia.com/chengjiao/nanshanzhongxin/'
+    r      = requests.get(url)
+    soup   = BeautifulSoup(r.content, 'lxml')
+    g_data = soup.find_all("ul", {"class", "listContent"})
+
+    for item in g_data[0].find_all('li'):
+        try:
+            yard  = item.find_all('div', {'class', 'title'})[0].text
+        except:
+            pass
+        try:
+            href  = item.find_all('a')[0].attrs['href']
+            result  = getTransactionDate(href)
+        except:
+            pass    
+
+        print(yard + '|' + result['total']+ '|' + result['date'])    
+
+
+
+# 成交具体详情（小区，总价，日期）
+def getTransactionDate(url):
+    req         = requests.get(url)
+    soup        = BeautifulSoup(req.content, 'lxml')
+    detail      = soup.find_all("div", {"class", "wrapper"})[0]
+    total_ele   = soup.find_all("span", {"class", "dealTotalPrice"})[0]
+
+
+    date        = detail.find_all('span')[0].text.split(' ')[0] # 成交日期
+    total_value = total_ele.find_all('i')[0].text + '万' # 成交金额
+
+    return ({'total': total_value, 'date': date})
+
+
+
+
+#getDoneTransaction()
+
+
+
+
+
+
+
+
+
+
+
+
+
+def visualize():
+    # 折线图画法
+    p = figure(plot_width=800, plot_height=300, title="abc")
+    p.multi_line(xs=[[1,2,3], [2,3,4]], ys=[[6,7,2], [4,5,7]], color=['red', 'black'])
+
+    
+
+    # 柱状图
+    plot = figure(plot_width=800, plot_height=300, title='rect')
+    plot.vbar(x=['a','b','c'], width=0.5, bottom=0, top=[1,2,3], color=['red', 'brown', 'navy'])
+
+    # pandas, source 是坐标,可以直接引入外部数据通过 bokeh.sampledata.autompg
+    source = ColumnDataSource(data={
+        'x': [1,2,3,4],
+        'y': [3,6,4,2],
+        })
+    p_panda = figure(plot_width=800, plot_height=300)
+    p_panda.circle('x', 'y', source=source)
+    
+    # x轴为label的柱状图展示
+    fruites=['a','b','c','d']
+    p_fruits = figure(x_range=fruites, plot_height=250, title='fruit')
+    p_fruits.vbar(x=fruites, top=[5,2,3,6], width=0.5)
+    p_fruits.xgrid.grid_line_color= 'red'
+    p_fruits.y_range.start = 0
+
+
+    #点图加注释LabelSet
+    source = ColumnDataSource(data=dict(
+        temp=[166, 171, 172, 168, 174, 162],
+        pressure=[165, 189, 220, 141, 260, 174],
+        names=['A', 'B', 'C', 'D', 'E', 'F']))
+
+    p_anotation = figure(x_range=(160, 175))
+    p_anotation.scatter(x='temp', y='pressure', size=8, source=source)
+    p_anotation.xaxis.axis_label = 'Temperature (C)'
+    p_anotation.yaxis.axis_label = 'Pressure (lbs)'
+
+    labels = LabelSet(x='temp', y='pressure', text='names', level='glyph',
+                  x_offset=5, y_offset=5, source=source, render_mode='canvas')
+
+    p_anotation.add_layout(labels)
+    show(p_anotation)
+
+#visualize() 
+
+    # 按品种为单位，三年的数量柱状图
+def groupChart():
+    fruits = ['Apples', 'Pears', 'Nectarines', 'Plums', 'Grapes', 'Strawberries']
+    years = ['2015', '2016', '2017']
+    data = {'fruits' : fruits,
+            '2015'   : [2, 1, 4, 3, 2, 4],
+            '2016'   : [5, 3, 3, 2, 4, 6],
+            '2017'   : [3, 2, 4, 4, 5, 3]}
+
+    # this creates [ ("Apples", "2015"), ("Apples", "2016"), ("Apples", "2017"), ("Pears", "2015), ... ]
+    x = [ (fruit, year) for fruit in fruits for year in years ]
+    counts = sum(zip(data['2015'], data['2016'], data['2017']), ()) # like an hstack
+
+    source = ColumnDataSource(data=dict(x=x, counts=counts))
+
+    p = figure(x_range=FactorRange(*x), plot_height=250, title="Fruit Counts by Year")
+
+    p.vbar(x='x', top='counts', width=0.9, source=source)
+
+    p.y_range.start = 0
+    p.x_range.range_padding = 0.1
+    p.xaxis.major_label_orientation = 1
+    p.xgrid.grid_line_color = None
+    show(p)
+#groupChart()
+
+# 混合图形，包含柱状图，折线图，并且按“季度”为group展开，折线图为4季度平均值连线
+def mixedCategory():
+    factors = [("Q1", "jan"), ("Q1", "feb"), ("Q1", "mar"),
+           ("Q2", "apr"), ("Q2", "may"), ("Q2", "jun"),
+           ("Q3", "jul"), ("Q3", "aug"), ("Q3", "sep"),
+           ("Q4", "oct"), ("Q4", "nov"), ("Q4", "dec")]
+    p = figure(x_range=FactorRange(*factors), plot_height=250)
+    x = [ 10, 12, 16, 9, 10, 8, 12, 13, 14, 14, 12, 16 ]
+    p.vbar(x=factors, top=x, width=0.9, alpha=0.5)       
+    qs, aves = ["Q1", "Q2", "Q3", "Q4"], [(10+12+16)/3, (9+10+8)/3, (12+13+14)/3, (14+12+16)/3]
+    p.line(x=qs, y=aves, color="red", line_width=3)
+    p.circle(x=qs, y=aves, line_color="red", fill_color="white", size=10)
+
+    p.y_range.start = 0
+    p.x_range.range_padding = 0.1
+    p.xgrid.grid_line_color = None
+    show(p)
+#mixedCategory()
+
+
+
+
+
+
 
 
 
